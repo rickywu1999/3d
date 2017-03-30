6898de24cc43680edda13d345b74ff7df6f6a36d
Ricky Wu <rickywu1999@gmail.com>
2017-03-29 22:41:12 -0400

    eh

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ transform = new_matrix()
 # print_matrix( make_hermite() )
 # print
 
-# parse_file( 'script', edges, transform, screen, color )
-parse_file( 'script2', edges, transform, screen, color )
+parse_file( 'script', edges, transform, screen, color )
+#parse_file( 'script2', edges, transform, screen, color )
